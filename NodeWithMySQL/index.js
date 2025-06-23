811f1412522938aea2c1e749413dc821646b3990
SHAIK RAIYAN <110379943+SHAIK-RAIYAN@users.noreply.github.com>
2025-06-24 01:28:40 +0530

    added user & edit path in index.js

--- NodeWithMySQL/index.js
+++ NodeWithMySQL/index.js
@@ -77,3 +77,72 @@ app.get("/", (req, res) => {
     res.send("Some error occured");
   }
 });
+
+//Users info page
+app.get("/users", (req, res) => {
+  let q = "select * from user";
+  try {
+    connection.query(q, (err, users) => {
+      if (err) {
+        throw err;
+      }
+      res.render("users", { users });
+    });
+  } catch (error) {
+    console.log(error);
+    res.send("Some error occured");
+  }
+});
+
+// go to edit page
+app.get("/user/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let q = `select * from user where id= '${id}';`;
+
+  try {
+    connection.query(q, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      let user = result[0];
+      res.render("edit", { user });
+    });
+  } catch (error) {
+    console.log(error);
+    res.send("Some error occured");
+  }
+});
+
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
+
+//edit user info
+app.patch("/user/:id", (req, res) => {
+  let { id } = req.params;
+  let q = `select * from user where id= '${id}';`;
+  let { uname: newusername, pass: password } = req.body;
+
+  try {
+    connection.query(q, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      let user = result[0];
+      console.log(user);
+      console.log(user.password);
+      if (password != user.password) {
+        res.send("password Incorrect");
+      } else {
+        let q2 = `UPDATE user SET username = '${newusername}' WHERE id = '${id}'`;
+        connection.query(q2, (err, result2) => {
+          if (err) throw err;
+          res.redirect("/users");
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.send("Some error occured");
+  }
+});
