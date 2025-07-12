ca1d2cd1418b11072b508841dfce56cfe4f9eb74
SHAIK RAIYAN <110379943+SHAIK-RAIYAN@users.noreply.github.com>
2025-07-12 10:54:45 +0530

    Update expressSession.js

--- Cookies&ExpressSessions/expressSession.js
+++ Cookies&ExpressSessions/expressSession.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 let sessionOptions = {
   secret: "superSecretCode",
@@ -9,6 +13,7 @@ let sessionOptions = {
 };
 
 app.use(session(sessionOptions));
+app.use(flash());
 
 app.listen(3000, () => {
   console.log(`listening on port : 3000`);
@@ -16,12 +21,17 @@ app.listen(3000, () => {
 });
 
 app.get("/register", (req, res) => {
-    // http://localhost:3000/register?name=Raiyan
-  let { username = "anonymous" } = req.query;
-  res.session.name = username; // creating variable 'name' in res.session
+  // http://localhost:3000/register?name=Raiyan
+  let { name = "anonymous" } = req.query;
+  req.session.name = name; // creating variable 'name' in req.session
+  req.flash("success", "user registered"); //key value par
+  req.flash("error", "some error occured!"); //key value par
   res.send(name);
 });
 
 app.get("/hello", (req, res) => {
-  res.send(`hello, ${res.session.name}`);
+  res.locals.successMsg = req.flash("success"); //property to set variables accessible in templates rendered with res.render.
+  res.render("page", { name: req.session.name });
+  //flash msg only shown once and after refresh no msg is shown
+  //sending msg using 'key'
 });
